aacfeed59e0a2e9d31cb51bb46e49eabfd8eca46
Michael <38651219+staehlmich@users.noreply.github.com>
2023-04-18 18:28:57 +0200

    Updated to format dataset and generate prompts for fine-tuning.

--- analyze.py
+++ analyze.py
@@ -4,19 +4,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.ticker import PercentFormatter
+import numpy as np
 
-if __name__ == '__main__':
-    flight_data = pd.read_csv("airlines.csv")
+#Link to dataset
+# https://www.kaggle.com/datasets/ulrikthygepedersen/airlines-delay
+
+def format_dataset(df):
+    """
+    Function to format the airlines.csv dataset.
+    :param df: pandas dataframe
+    :return: formatted pandas dataframe
+    """
+    #Drop columns not relevant for project
+
     #Renaming Delay
-    flight_data['Delay'] = flight_data['Delay'].map({0: 'no', 1: 'yes'})
+    df['Delay'] = df['Class'].map({0: 'no', 1: 'yes'})
+    df = df.drop(['Class'], axis=1)
     #Renaming DayOfWEek and ordering
     days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    flight_data['DayOfWeek'] = pd.Categorical(flight_data['DayOfWeek'].map({1: 'Mon', 2: 'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat', 7:'Sun'}), days)
+    df['DayOfWeek'] = pd.Categorical(df['DayOfWeek'].map({1: 'Mon', 2: 'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat', 7:'Sun'}), days)
+    #Departure time in hours (am/pm)
+    df['Time'] = (df['Time']/60).round(2)
+
+    return df
+
+if __name__ == '__main__':
+    flight_data = pd.read_csv("data/airlines_delay.csv")
+
     #Colum names
-    # print(flight_data.columns)
+    print(flight_data.columns)
+    print(flight_data.shape)
     #Numer of airlines
-    # print(flight_data["Airline"].nunique())
+    print(flight_data["Airline"].nunique())
+    print(flight_data["Length"].iloc[0:5])
+    #Formating dataset
+    flight_data = format_dataset(flight_data)
 
     #Show number of delayed flights
     # plt.title("Number of delayed flights")
@@ -40,17 +62,27 @@ if __name__ == '__main__':
     #Delayed flights by length
 
     #Delayed flights by weekday
-    sns.set_style("whitegrid")
-    chart = sns.histplot(data=flight_data, x="DayOfWeek", hue="Delay", multiple="stack", discrete=True)
-    for bars in chart.containers:
-        heights = [b.get_height() for b in bars]
-        labels = [f'{h:.1f}%' if h > 0.001 else '' for h in
-                  heights]
-        chart.bar_label(bars, labels=labels, label_type='center')
+    # sns.set_style("whitegrid")
+    # chart = sns.histplot(data=flight_data, x="DayOfWeek", hue="Delay", multiple="stack", discrete=True)
+    # for bars in chart.containers:
+    #     heights = [b.get_height() for b in bars]
+    #     labels = [f'{h:1f}%' if h > 0.001 else '' for h in
+    #               heights]
+    #     chart.bar_label(bars, labels=labels, label_type='center')
     # chart.yaxis.set_major_formatter(PercentFormatter(1))
+
+    #This is the same as the code above. Something is wrong with % formatting.
+    # for p in chart.patches:
+    #     txt = str(int(p.get_height())) + '%'
+    #     txt_x = p.get_x()
+    #     txt_y = p.get_height()
+    #     chart.text(txt_x, txt_y, txt)
+
+    # Show the plot
+    # plt.show()
 
     # chart.fig.subplots_adjust(top=.95)
     # chart.set_axis_labels("Day of Week", "Number of Delays")
     # chart.ax.set_title("Flights by weekday")
 
-    plt.show()
+    # plt.show()
--- llm_fly.py
+++ llm_fly.py
@@ -4,10 +4,28 @@
 import openai
 import config
 import pandas as pd
+from analyze import format_dataset
+import json
 
 openai.api_key = config.OPENAI_API_KEY
 
+def generate_prompts(df):
+    """
+    Function to generate completion prompts from df for fine-tuning LLM.
+    :param df: pandas dataframe with airline data
+    :return:
+    """
+    # Iterate through each row in the dataframe and create a prompt and completion pair
+    prompt_completion_pairs = []
+    for index, row in df.iterrows():
+        prompt = f"Provide a written summary of Flight {row['Flight']}, operated by {row['Airline']}, which departed from {row['AirportFrom']} to {row['AirportTo']} on {row['DayOfWeek']} at {row['Time']} hours and had a flight length of {row['Length']} minutes."
+        completion = f"Flight {row['Flight']} operated by {row['Airline']} departed from {row['AirportFrom']} to {row['AirportTo']} on {row['DayOfWeek']} at {row['Time']} hours and had a flight length of {row['Length']} minutes. The delay status was {row['Delay']}."
+        prompt_completion_pairs.append(
+            {"prompt": prompt, "completion": completion})
 
+    # Export the prompt and completion pairs to a JSON file
+    with open("data/prompt_completion_pairs.json", "w") as outfile:
+        json.dump(prompt_completion_pairs, outfile)
 
 if __name__ == '__main__':
 
@@ -20,6 +38,7 @@ if __name__ == '__main__':
     # Response without fine-tuning:
     #"\n\nIt is not possible to answer this question without more information."
 
-    # df = pd.read_csv("airlines.csv")
-    # print(df.columns)
-    # print(df.iloc[1])
+    df = pd.read_csv("data/airlines_delay.csv")
+    df = format_dataset(df)
+
+    # generate_prompts(df)
